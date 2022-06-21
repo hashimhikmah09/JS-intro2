81bdf48724d93645a4ffd2898b2aba9e8885bb9a
Hikmah <hashimhikmah09@gmail.com>
2022-06-21 12:59:15 +0100

    first Js Calc.

---
+++ index.js
@@ -0,0 +1,20 @@
+a = prompt(' Enter first number')
+b = prompt (' Enter second number')
+c = prompt('Enter operator from the list below +,-,*,/,%')
+
+if (c =="+")
+{
+    alert (a + b)
+}
+else if (c =="-")
+{
+    alert (a - b)
+}
+else if (c =="*")
+{
+    alert (a * b)
+}
+else if (c =="/")
+{
+    alert (a / b)
+}
